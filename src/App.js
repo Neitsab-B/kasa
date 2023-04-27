fc4e438f3aa4ec13bd1f7824ce1f8d47eb92a81c
Neitsab-B <bbarbe.pro@gmail.com>
2023-04-27 10:35:02 +0200

    pages + routes update
    -add files pages
    -route system operationnal
    -add first files components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import LogementProfile from "./pages/ApartmentProfile";
+import Error from "./pages/Error";
+
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apartment/:id" element={<LogementProfile />}></Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ApartmentProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApartmentProfile = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ApartmentProfile;
