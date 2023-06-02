d0baea5901c5432674b53ec6f80a2beda703f5b4
Neitsab-B <bbarbe.pro@gmail.com>
2023-06-02 15:01:44 +0200

    error 404 update
    add a if else to move the user at the page 404 if the current apartment ID is wrong, using the hook useNavigate

--- src/pages/ApartmentProfile.js
+++ src/pages/ApartmentProfile.js
@@ -1,32 +1,38 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, useEffe } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import ApartmentInfos from "../components/ApartmentInfos/ApartmentInfos";
 import Header from "../components/Header/Header.js";
-import Footer from "../components/Footer/Footer.js"
-import ImageSlider from "../components/ImageSlider/ImageSlider"
+import Footer from "../components/Footer/Footer.js";
+import ImageSlider from "../components/ImageSlider/ImageSlider";
 
 const ApartmentProfile = () => {
+  //utilisation du hook useNavigate pour la redirection vers la page 404
+  let navigate = useNavigate();
+
   const { data } = useFetch("../apartments.json");
   const { id } = useParams();
 
   const arrOfData = [];
   Object.keys(data).forEach((key) => {
     arrOfData.push(data[key]);
-    console.log(arrOfData);
   });
 
   const currentApartment = arrOfData.find((d) => d.id === id);
-  console.log(currentApartment);
 
-  return (
-    <>
-      <Header />
-      {currentApartment && <ImageSlider props={currentApartment.pictures} /> }
-      {currentApartment && <ApartmentInfos props={currentApartment} />}
-      <Footer />
-    </>
-  );
+  // rajout d'un if else pour rediriger la page vers 404 en cas d'erreur dans l'ID du logement
+  if (currentApartment) {
+    return (
+      <>
+        <Header />
+        {currentApartment && <ImageSlider props={currentApartment.pictures} />}
+        {currentApartment && <ApartmentInfos props={currentApartment} />}
+        <Footer />
+      </>
+    );
+  } else {
+    navigate("./404");
+  }
 };
 
 export default ApartmentProfile;
